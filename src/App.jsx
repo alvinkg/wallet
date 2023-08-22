bddef62c358e09e5f05852b9cc98a4b1c6d0b204
= <alvin@konvergentgroup.com>
2023-08-22 13:04:30 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+// import { Routes, Route, Outlet, Link } from "react-router-dom";
+import './css/styles.css';
+
+// import logo from './logo/Soccerball.svg';
+
+// import FixedButton from './FixedButton';
+// import Test from './Test';
+// import Fa from './Fa';
+// import Qrscan from './Qrscan';
+// import PriceTag from './PriceTag';
+import EWallet from './components/EWallet';
+// import QrReader from './QrReader';
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+// import { DATA } from './constants';
+
+export default function App() {
+  return (
+    <div>
+      <h1>My EWallet</h1>
+      <EWallet/>
+    </div>
+  )
+}
